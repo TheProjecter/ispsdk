42f0db95f0270f44840e2241414e424912572ac3
IrkDeniska <IrkDeniska@3eca520e-e11c-11dd-9ba5-89a75009fd5d>
2009-01-15 03:22:57 +0000

    Start revision

---
+++ trunk/config.php
@@ -0,0 +1,9 @@
+<?php
+  $CONFIG['dbhost'] = 'localhost';
+  $CONFIG['dbname'] = 'fwsr';
+  $CONFIG['dbuser'] = 'root';
+  $CONFIG['dbpassword'] = 'root';
+  $CONFIG['dbpref'] = '';
+  $CONFIG['template_file'] = 'template.html';
+  $CONFIG['maintitle'] = 'Statistic';
+?>
