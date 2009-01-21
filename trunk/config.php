556a80b92348d534d18d0e2fccfcd9b19110072a
sergeyreym <sergeyreym@3eca520e-e11c-11dd-9ba5-89a75009fd5d>
2009-01-21 08:36:42 +0000

    NULL-values in table now with background image "null" and Integer-values in table text-align: right

--- trunk/config.php
+++ trunk/config.php
@@ -2,7 +2,7 @@
   $CONFIG['dbhost'] = 'localhost';
   $CONFIG['dbname'] = 'fwsr';
   $CONFIG['dbuser'] = 'root';
-  $CONFIG['dbpassword'] = 'root';
+  $CONFIG['dbpassword'] = '';
   $CONFIG['dbpref'] = '';
   $CONFIG['template_file'] = 'template.html';
   $CONFIG['maintitle'] = 'Statistic';
